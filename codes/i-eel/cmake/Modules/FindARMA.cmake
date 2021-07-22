7e55fc3c587b5ac4a259983549951fdf348d160f
BEC Jeremie <jeremie.bec@inria.fr>
2021-07-22 09:54:30 +0000

    Modified to find ARMA on NEF

--- codes/i-eel/cmake/Modules/FindARMA.cmake
+++ codes/i-eel/cmake/Modules/FindARMA.cmake
@@ -2,7 +2,11 @@
 # ARMA_LIBRARIES = libarmadillo
 # ARMA_FOUND = true if ARMA is found
 
+# To find Armadillo on mac, use the following line
 FIND_PATH(ARMA_LIBDIR libarmadillo.dylib HINTS $ENV{LD_LIBRARY_PATH} "/usr/local/lib/" "/opt/local/lib/")
+# To find Armadillo on the NEF cluster, comment the above line and uncomment the following:
+# FIND_PATH(ARMA_LIBDIR libarmadillo.so HINTS $ENV{LD_LIBRARY_PATH} "/home/jbec/Info/lib64/")
+
 # if(ARMA_LIBDIR)
 # else()
 #   set(ARMA_LIBDIR "/usr/local/lib/")
@@ -10,7 +14,11 @@ FIND_PATH(ARMA_LIBDIR libarmadillo.dylib HINTS $ENV{LD_LIBRARY_PATH} "/usr/local
 MESSAGE(STATUS "ARMA_LIB_DIR=${ARMA_LIBDIR}")
 
 FIND_PATH(ARMA_INCLUDE_DIR arma_version.hpp HINTS "${ARMA_LIBDIR}/../include/armadillo_bits/")
+# For mac:
 set(ARMA_INCLUDE_DIR "/usr/local/include/")
+# For NEF:
+# set(ARMA_INCLUDE_DIR "/home/jbec/Info/include/")
+
 FIND_LIBRARY(ARMA_LIBRARY armadillo HINTS ${ARMA_LIBDIR})
 get_filename_component(ARMA_LIBDIR ${ARMA_LIBRARY} PATH)
 include(LibFindMacros)
