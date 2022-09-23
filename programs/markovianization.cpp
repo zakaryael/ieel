d0085377680d231915c1db71e5ec6c2e8b8e459d
Jeremie Bec <jeremie.bec@inria.fr>
2022-09-23 09:25:05 +0200

    pushed the right version of markovianisation

--- programs/markovianization.cpp
+++ programs/markovianization.cpp
@@ -16,13 +16,11 @@ int main(int argc, char* argv[]) {
     args.section("Program options");
     const string outdir = args.getpath("-o", "--outdir", "output_data/untiteled/", "output directory");
     const string indir = args.getpath("-in", "--indir", "", "input directory to a binary file from which initial Q is read");
-    const string Qinitdir = args.getpath("-Qdir", "--Qinitdir", "", "input directory to a csv file from which the initial Q is read");
-    const string Pinitdir = args.getpath("-Pidir", "--Pinitdir", "", "input directory to a csv file from which the initial policy is read");  
+    const string PiFile = args.getpath("-Pi", "--PiFile", "", "csv file from which the initial policy is read");  
     const double L = args.getreal("-L", "--length", 1.0, "fiber length");
     const double zeta = args.getreal("-z", "--zeta", 2.5e3, "friction coefficient");
     const double E = args.getreal("-E", "--EI", 1.0, "Young modulus");
     const double beta = args.getreal("-beta", "--penalisation", 400, "penalisation of extensibility");
-    const double Tmax = args.getreal("-T", "--time",1.0e7, "integration time in multiples of nl");
     const double dt = args.getreal("-dt", "--timestep", 1e-3, "time step");
     const int Ns = args.getint("-ns", "--Ns", 200, "number of points in the fiber's discretization");
     const int k = args.getint("-k", "--wavenumber", 2, "Forcing wavenumber");
@@ -32,8 +30,9 @@ int main(int argc, char* argv[]) {
     const int noflow = args.getint("-nfl", "--noflow", 0, "Input 1 for no flow 0 for cellular flow"); 
     const int incl_buckl = args.getint("-bckl", "--incl_buckl", 0, "Input 1 to include buckled states 0 otherwise");
     const double u0 = args.getreal("-slim", "--speed", 0.1, "Vitesse limite");
-    const int Nlearning = args.getint("-nl", "--step_learning", 100, "learning period (in number of timesteps)");
-
+    const int Nlearn = args.getint("-nl", "--step_learning", 100, "learning period (in number of timesteps)");
+    const int Ntot = args.getint("-Ntot", "--num_step",10000, "integration time in units of Nlearn*dt");
+    
     args.check();
     mkdir(outdir);
     args.save(outdir);
@@ -46,14 +45,16 @@ int main(int argc, char* argv[]) {
     
 
 
+    // define the learning
+    vector<int> PiVec = {3,3,3,3,6,6};
     Fiber2D Fib(Ns, L, zeta, E, beta);
+    vector <Fiber2D> FibTab(7);
     // set the forcing
     cout<<endl<<"------------------------------------------------"<<endl;
     cout<<"k="<<k<<"\tom="<<om<<"\talpha="<<alpha<<endl;
     Fib.setforcing(k, om);
     
-    // define the learning
-
+    
     MyCol Ampl;
     double a0 = zeta*alpha*om/(2.0*M_PI*(double)k/L);
     Ampl.set_size(4);
@@ -72,63 +73,49 @@ int main(int argc, char* argv[]) {
     
     // time loop
     cout<<endl<<"------------------------------------------------"<<endl;
-    //int nstep = round(1/dt) / ;
-    cout<<"output every 1 sec"<<endl;
-    int it = 0;
-    double time = 0;
-    double timer;
-    for (int n=0; n < Tmax; n++) 
-    {  
-        for(int a=0; a < 7; a++){
-            
-        FILE *fout = fopen(cname,"a");
-        // Appends 6 doubles to the file: time, position, current_state, action, next_state, reward.
-        double tmp = (double)n;
-        fwrite(&tmp, sizeof(double), 1, fout);
-        fwrite(&time, sizeof(double), 1, fout);
-
-        QL.set_action_to(a);
-
-        tmp = (double)a;
-        fwrite(&tmp, sizeof(double), 1, fout);
-
-        QL.update_forcing();
-        Fib.setforcing(QL.getp(), QL.getA());
-        Fib.read(U, indir+"fiber"+i2s(n * Nlearning)+".ff");
-        
-        int current_state = QL.compute_state(Fib.wind(U), Fib.orientation(), incl_buckl * Fib.calc_buckle());
-        cout<<"t = "<< time <<setw(10)<<"X = " << setprecision(4)<<Fib.getcenter(0)<<setw(10)<<"Vx = "<<Fib.getvelocity(0) << setprecision(1) << setw(10) << " State: "<< current_state<<endl;
-        cout<<"Action: "<< QL.getaction()<<endl;
+    for (int n=0; n < Ntot; n++) 
+    {
         double xold = Fib.getcenter(0);
-        fwrite(&xold, sizeof(double), 1, fout);
-
-        tmp = (double)current_state;
-        fwrite(&tmp, sizeof(double), 1, fout);
-
-        it = 0;
-        timer = 0;
-        while(it < Nlearning){
-            Fib.evol(dt,U);
-            it++;
-            timer = timer + dt;
-        }
-
-        int next_state = QL.compute_state(Fib.wind(U), Fib.orientation(), incl_buckl * Fib.calc_buckle()); 
-        cout<<"t = "<<n+1<<setw(10)<<"X = "<< setprecision(4)<<Fib.getcenter(0)<<setw(10)<<"Vx = "<<Fib.getvelocity(0) << setprecision(1) << setw(10) << " State: "<< next_state<<endl; 
+        int oldstate = QL.compute_state(Fib.wind(U), Fib.orientation(), incl_buckl * Fib.calc_buckle());
         
-        tmp = (double)next_state;
-        fwrite(&tmp, sizeof(double), 1, fout);
-
-        tmp = Fib.getcenter(0);
-        fwrite(&tmp, sizeof(double), 1, fout);
-        tmp = tmp - xold;
-        cout << "reward: " << tmp << endl;
-        fwrite(&tmp, sizeof(double), 1, fout);
-        fclose(fout);
-        cout << "__________________" << endl;
+        for(int a=0; a<7; a++) {
+            FibTab.at(a) = Fib;
+            QL.set_action_to(a);
+            QL.update_forcing();
+            FibTab.at(a).setforcing(QL.getp(), QL.getA());
+            for(int it=0; it<Nlearn; ++it) {
+                FibTab.at(a).evol(dt,U);
+            }
+            // Outputs
+            FILE *fout = fopen(cname,"a");
+            // 0: step number
+            double tmp = (double)n;
+            fwrite(&tmp, sizeof(double), 1, fout);
+            // 1: time
+            tmp = (double)(n*Nlearn)*dt;
+            fwrite(&tmp, sizeof(double), 1, fout);
+            // 2: action
+            tmp = (double)a;
+            fwrite(&tmp, sizeof(double), 1, fout);
+            // 3: former position of the center of mass
+            fwrite(&xold, sizeof(double), 1, fout);
+            // 4: former state
+            tmp = (double)oldstate;
+            fwrite(&tmp, sizeof(double), 1, fout);
+            // 5: new state
+            tmp = (double)QL.compute_state(FibTab.at(a).wind(U), FibTab.at(a).orientation(), incl_buckl * FibTab.at(a).calc_buckle());
+            fwrite(&tmp, sizeof(double), 1, fout);
+            // 6: new position of the center of mass
+            tmp = FibTab.at(a).getcenter(0);
+            fwrite(&tmp, sizeof(double), 1, fout);
+            // 7: reward
+            tmp = tmp - xold;
+            fwrite(&tmp, sizeof(double), 1, fout);
+            fclose(fout);
         }
-        cout << "------------------" << endl << endl;
-        time = time + timer;
+
+        Fib = FibTab.at(PiVec.at(oldstate));
     }
-    return 1;
+cout << "__________________" << endl;
+return 1;
 }
